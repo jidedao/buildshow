c6628e6af8a951e154e701528f259c02ac25a500
qinhao <digiarty001@qinhao-iMac.local>
2014-06-05 18:44:20 +0800

    户型

---
+++ ShiMao/QHHouseTypeView.h
@@ -0,0 +1,18 @@
+//
+//  QHHouseTypeView.h
+//  ShiMao
+//
+//  Created by qinhao on 14-6-5.
+//  Copyright (c) 2014年 qinhao. All rights reserved.
+//
+
+#import "HLCustomOriginView.h"
+
+@interface QHHouseTypeView : HLCustomOriginView
+
+@property (nonatomic,assign) IBOutlet UIButton *btnPingMian;
+@property (nonatomic,assign) IBOutlet UIButton *btn3d;
+@property (nonatomic,assign) IBOutlet UIButton *btnQuanJing;
+@property (nonatomic,assign) IBOutlet UIButton *btnFangDai;
+
+@end
---
+++ ShiMao/QHHouseTypeView.mm
@@ -0,0 +1,31 @@
+//
+//  QHHouseTypeView.m
+//  ShiMao
+//
+//  Created by qinhao on 14-6-5.
+//  Copyright (c) 2014年 qinhao. All rights reserved.
+//
+
+#import "QHHouseTypeView.h"
+
+@implementation QHHouseTypeView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
